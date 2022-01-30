aff25e36e11c428d31d29e4993e961e5ce54a076
Eric Laukien <ericlaukien@gmail.com>
2022-01-30 17:41:31 -0500

    Some small optimizations

--- src/scaler.c
+++ src/scaler.c
@@ -25,26 +25,31 @@ void scale_nearest_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i32
 void scale_bilinear_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i32 dst_width, i32 dst_height) {
     f32 ratio_x = (f32)(src_width - 1) / (f32)dst_width;
     f32 ratio_y = (f32)(src_height - 1) / (f32)dst_height;
+    i32 src_height4 = src_height * 4;
 
     for (i32 dst_x = 0; dst_x < dst_width; dst_x++) {
         f32 src_x_f = (dst_x + 0.5f) * ratio_x;
         i32 src_x = (i32)src_x_f;
         f32 interp_x = src_x_f - src_x;
 
+        __m128 ix = _mm_set1_ps(interp_x);
+        __m128 ix1 = _mm_set1_ps(1.0f - interp_x);
+
+        i32 dst_offset4 = 4 * dst_height * dst_x;
+        i32 src_offset4 = 4 * src_height * src_x;
+
         for (i32 dst_y = 0; dst_y < dst_height; dst_y++) {
             f32 src_y_f = (dst_y + 0.5f) * ratio_y;
             i32 src_y = (i32)src_y_f;
             f32 interp_y = src_y_f - src_y;
 
-            i32 dst_start = 4 * (dst_y + dst_height * dst_x);
+            i32 dst_start = 4 * dst_y + dst_offset4;
 
-            i32 src_start00 = 4 * (src_y + src_height * src_x);
+            i32 src_start00 = 4 * src_y + src_offset4;
             i32 src_start01 = src_start00 + 4;
-            i32 src_start10 = src_start00 + src_height * 4;
+            i32 src_start10 = src_start00 + src_height4;
             i32 src_start11 = src_start10 + 4;
 
-            __m128 ix = _mm_set1_ps(interp_x);
-            __m128 ix1 = _mm_set1_ps(1.0f - interp_x);
             __m128 iy = _mm_set1_ps(interp_y);
             __m128 iy1 = _mm_set1_ps(1.0f - interp_y);
 
@@ -68,26 +73,31 @@ void scale_bilinear_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i3
 void scale_bilinear_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i32 dst_width, i32 dst_height) {
     f32 ratio_x = (f32)(src_width - 1) / (f32)dst_width;
     f32 ratio_y = (f32)(src_height - 1) / (f32)dst_height;
+    i32 src_height4 = src_height * 4;
 
     for (i32 dst_x = 0; dst_x < dst_width; dst_x++) {
         f32 src_x_f = (dst_x + 0.5f) * ratio_x;
         i32 src_x = (i32)src_x_f;
         f32 interp_x = src_x_f - src_x;
 
+        float32x4_t ix = vdupq_n_f32(interp_x);
+        float32x4_t ix1 = vdupq_n_f32(1.0f - interp_x);
+
+        i32 dst_offset4 = 4 * dst_height * dst_x;
+        i32 src_offset4 = 4 * src_height * src_x;
+
         for (i32 dst_y = 0; dst_y < dst_height; dst_y++) {
             f32 src_y_f = (dst_y + 0.5f) * ratio_y;
             i32 src_y = (i32)src_y_f;
             f32 interp_y = src_y_f - src_y;
 
-            i32 dst_start = 4 * (dst_y + dst_height * dst_x);
+            i32 dst_start = 4 * dst_y + dst_offset4;
 
-            i32 src_start00 = 4 * (src_y + src_height * src_x);
+            i32 src_start00 = 4 * src_y + src_offset4;
             i32 src_start01 = src_start00 + 4;
-            i32 src_start10 = src_start00 + src_height * 4;
+            i32 src_start10 = src_start00 + src_height4;
             i32 src_start11 = src_start10 + 4;
 
-            float32x4_t ix = vdupq_n_f32(interp_x);
-            float32x4_t ix1 = vdupq_n_f32(1.0f - interp_x);
             float32x4_t iy = vdupq_n_f32(interp_y);
             float32x4_t iy1 = vdupq_n_f32(1.0f - interp_y);
 
@@ -115,25 +125,29 @@ void scale_bilinear_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i3
 void scale_bilinear_4f32(f32 src[], f32 dst[], i32 src_width, i32 src_height, i32 dst_width, i32 dst_height) {
     f32 ratio_x = (f32)(src_width - 1) / (f32)dst_width;
     f32 ratio_y = (f32)(src_height - 1) / (f32)dst_height;
+    i32 src_height4 = src_height * 4;
 
     for (i32 dst_x = 0; dst_x < dst_width; dst_x++) {
         f32 src_x_f = (dst_x + 0.5f) * ratio_x;
         i32 src_x = (i32)src_x_f;
         f32 interp_x = src_x_f - src_x;
+        f32 interp_x1 = 1.0f - interp_x;
+
+        i32 dst_offset4 = 4 * dst_height * dst_x;
+        i32 src_offset4 = 4 * src_height * src_x;
 
         for (i32 dst_y = 0; dst_y < dst_height; dst_y++) {
             f32 src_y_f = (dst_y + 0.5f) * ratio_y;
             i32 src_y = (i32)src_y_f;
             f32 interp_y = src_y_f - src_y;
 
-            i32 dst_start = 4 * (dst_y + dst_height * dst_x);
+            i32 dst_start = 4 * dst_y + dst_offset4;
 
-            i32 src_start00 = 4 * (src_y + src_height * src_x);
+            i32 src_start00 = 4 * src_y + src_offset4;
             i32 src_start01 = src_start00 + 4;
-            i32 src_start10 = src_start00 + src_height * 4;
+            i32 src_start10 = src_start00 + src_height4;
             i32 src_start11 = src_start10 + 4;
 
-            f32 interp_x1 = 1.0f - interp_x;
             f32 interp_y1 = 1.0f - interp_y;
 
             f32 pr0 = interp_x1 * src[src_start00    ] + interp_x * src[src_start10    ];
