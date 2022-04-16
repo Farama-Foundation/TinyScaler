bbdf2a06d758a4dc4f5164c8e9630c8955dbfa7d
Eric Laukien <ericlaukien@gmail.com>
2022-04-16 15:44:42 -0400

    1.0.1

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='tinyscaler',
-    version='1.0.0',
+    version='1.0.1',
     description='A tiny, simple image scaler',
     long_description='https://github.com/Farama-Foundation/TinyScaler',
     install_requires=[
